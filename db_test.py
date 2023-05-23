d5cb6ebf75cebb6410144d5c9131f807acfd643a
Lara Tiziana Nowotsch <tiziana_nowotsch@web.de>
2023-05-23 19:54:58 +0200

    Datenbank und Skript hinzugefügt

---
+++ db_test.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+Testdaten = [["2023-05-23", 5], ["2023-05-23", 12], ["2023-05-23", 17], ["2023-05-24", 18]]
+
+connect = sqlite3.connect("datenbank.db")
+cursor = connect.cursor()
+
+# cursor.execute("CREATE TABLE IF NOT EXISTS Temperaturen(Datum date, Temperatur int)")
+#
+# for Datum, Temperatur in Testdaten:
+#     cursor.execute("INSERT INTO Temperaturen(Datum, Temperatur) VALUES (?, ?)", (Datum, Temperatur))
+
+befehl = cursor.execute("SELECT MIN(Temperatur), MAX(Temperatur), AVG(Temperatur) FROM Temperaturen")
+print(cursor.fetchall())
+
+connect.commit()
+connect.close()
